0abe06ba0abaf19315cade12b98d7c533953cfdc
SasaKuruppuarachchi <sasa.kuruppuarachchi@gmail.com>
2024-06-21 02:34:05 +0300

    fix key control to match gamepad

--- px4_ros2_offboard/control.py
+++ px4_ros2_offboard/control.py
@@ -35,10 +35,10 @@ moveBindings = {
     's': (0, 0, -1, 0),#Z-
     'a': (0, 0, 0, -1), #Yaw+
     'd': (0, 0, 0, 1),#Yaw-
-    '\x1b[A' : (0, 1, 0, 0),  #Up Arrow
-    '\x1b[B' : (0, -1, 0, 0), #Down Arrow
-    '\x1b[C' : (-1, 0, 0, 0), #Right Arrow
-    '\x1b[D' : (1, 0, 0, 0),  #Left Arrow
+    '\x1b[A' : (-1, 0, 0, 0),  #Up Arrow
+    '\x1b[B' : (1, 0, 0, 0), #Down Arrow
+    '\x1b[C' : (0, -1, 0, 0), #Right Arrow
+    '\x1b[D' : (0, 1, 0, 0),  #Left Arrow
 }
 
 
